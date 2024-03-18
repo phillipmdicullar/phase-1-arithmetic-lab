cbe67b127867e30122e8910c0de0856cd332dccd
Philip <phillip.emdokolo@student.moringaschool.com>
2024-03-19 00:16:12 +0300

    arithmetic operators ...(spread operator)

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 *= num2;
+console.log(multiply)
+let random = Math.floor(Math.random()*10);
+console.log(random)
+let num3 = 20
+let num4 = 5
+let mod = num3 / num4
+console.log(mod)
+let numbers = [10,5,15,20];
+let max = Math.max(...numbers)
+console.log(max)
